98f6441bff7c15060e01890858541cfe6ff9a57b
Oussama Olap <o.adrouj@gmail.com>
2022-12-29 17:57:00 +0100

    PusherBeams Update

--- src/android/PusherBeams.java
+++ src/android/PusherBeams.java
@@ -1,30 +1,126 @@
 package com.evidence;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.content.Context;
-import android.widget.Toast;
+
+import com.pusher.pushnotifications.BeamsCallback;
+import com.pusher.pushnotifications.PushNotifications;
+import com.pusher.pushnotifications.PusherCallbackError;
+import com.pusher.pushnotifications.auth.AuthData;
+import com.pusher.pushnotifications.auth.AuthDataGetter;
+import com.pusher.pushnotifications.auth.BeamsTokenProvider;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class PusherBeams extends CordovaPlugin {
+    private static final String TAG = "PUSHER";
+    JSONObject json = null;
+
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("show".equals(action)) {
-        show(args.getString(0), callbackContext);
-        return true;
-        }
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+        try {
+            if (action.equals("setUserId")) {
+                String tokenUrl = data.getString(0);
+                String userId = data.getString(1);
+                String authToken = data.getString(2);
 
-        return false;
+                if (tokenUrl == null || userId == null || authToken == null) {
+                    callbackContext.error("Please provide all the information required");
+                }
+                registerUserId(tokenUrl, userId, authToken, callbackContext);
+                return true;
+            } else if (action.equalsIgnoreCase("start")) {
+                String instanceId = data.getString(0);
+
+                if (instanceId == null) {
+                    callbackContext.error("Please provide InstanceID");
+                }
+                startPusher(instanceId);
+                callbackContext.success("OK");
+                return true;
+            } else if (action.equalsIgnoreCase("clear")) {
+                PushNotifications.clearAllState();
+                callbackContext.success("OK");
+                return true;
+            } else if (action.equalsIgnoreCase("getNotification")) {
+                callbackContext.success(json);
+                json = null;
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            callbackContext.error(e.toString());
+            e.printStackTrace();
+        } finally {
+            return true;
+        }
     }
 
-    private void show(String msg, CallbackContext callbackContext) {
-        if (msg == null || msg.length() == 0) {
-            callbackContext.error("Empty message!");
-        } else {
-            Toast.makeText(webView.getContext(), msg, Toast.LENGTH_LONG).show();
-            callbackContext.success(msg);
-        }
+    private void registerUserId(String tokenUrl, String userId, String authToken, CallbackContext cb) {
+        BeamsTokenProvider tokenProvider = new BeamsTokenProvider(tokenUrl,
+                () -> {
+                    // Headers and URL query params your auth endpoint needs to
+                    // request a Beams Token for a given user
+                    HashMap<String, String> headers = new HashMap<>();
+                    // for example:
+                    headers.put("Authorization", "Bearer " + authToken);
+                    HashMap<String, String> queryParams = new HashMap<>();
+                    return new AuthData(
+                            headers,
+                            queryParams);
+                });
+
+        PushNotifications.setUserId(userId, tokenProvider, new BeamsCallback<Void, PusherCallbackError>() {
+            @Override
+            public void onSuccess(Void... values) {
+                cb.success("OK");
+                Log.v("", "success");
+            }
+
+            @Override
+            public void onFailure(PusherCallbackError error) {
+                cb.error("failed to set user id");
+                Log.v("", "fail");
+            }
+        });
     }
+
+    private void startPusher(String instanceId) {
+        Context context = this.cordova.getActivity().getApplicationContext();
+        PushNotifications.start(context, instanceId);
+
+        Log.d(TAG, "STARTING PUSHER");
+
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        Log.d(TAG, "onNewIntent - starting");
+        Bundle extras = intent.getExtras();
+
+        if (extras != null) {
+            json = new JSONObject();
+            Set<String> keys = extras.keySet();
+            for (String key : keys) {
+                try {
+                    json.put(key, JSONObject.wrap(extras.get(key)));
+                } catch (JSONException e) {
+                    // Handle exception here
+                }
+            }
+        }
+
+    }
+
 }
--- www/PusherBeams.js
+++ www/PusherBeams.js
@@ -1,4 +1,4 @@
-var exec = cordova.require('cordova/exec');
+/* var exec = cordova.require('cordova/exec');
 
 var PusherBeams = function() {
 console.log('PusherBeams instanced');
@@ -14,8 +14,38 @@ onSuccess(obj);
 };
 
 exec(successCallback, errorCallback, 'PusherBeams', 'show', [msg]);
+}; */
+
+
+const PusherBeamsCDV = {
+  getRegistrationState: (successCb, errorCb) => {
+    cordova.exec(successCb, errorCb, "PusherBeams", "getRegistrationState", []);
+  },
+
+  start: (instanceId, successCb, errorCb) => {
+    cordova.exec(successCb, errorCb, "PusherBeams", "start", [instanceId]);
+  },
+
+  setUserId: (tokenUrl, userId, authToken, successCb, errorCb) => {
+    cordova.exec(successCb, errorCb, "PusherBeams", "setUserId", [
+      tokenUrl,
+      userId,
+      authToken,
+    ]);
+  },
+
+  clear: (successCb, errorCb) => {
+    cordova.exec(successCb, errorCb, "PusherBeams", "clear", []);
+  },
+
+  getNotification: (successCb, errorCb) => {
+    cordova.exec(successCb, errorCb, "PusherBeams", "getNotification", []);
+  },
+
 };
 
-if (typeof module != 'undefined' && module.exports) {
+/* if (typeof module != 'undefined' && module.exports) {
 module.exports = PusherBeams;
-}
+} */
+
+module.exports = PusherBeamsCDV;
